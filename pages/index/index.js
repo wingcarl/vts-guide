0d772bd24cd7cc2cc844a448446e41adfff3bdcc
wingcarl <191131600@qq.com>
2018-09-02 07:37:09 +0800

    wind and tide chart

--- pages/index/index.js
+++ pages/index/index.js
@@ -2,9 +2,42 @@ import * as echarts from '../../ec-canvas/echarts';
 
 const app = getApp();
 var sliderWidth = 96; // 需要设置slider的宽度，用于计算中间位置
+var chart = null
+var chartCj = null
+var water = null
+var widthL;
+var heightL;
+function initWater(canvas, width, height) {
+  water = echarts.init(canvas, null, {
+    width: widthL,
+    height: heightL
+  });
+  canvas.setChart(water);
 
+  var option = {
+    title: {
+      text: '水位潮汐查看',
+      left: 'center'
+    },
+    legend: {
+      data: ['港务集团水位','江阴潮汐','天生港潮汐'],
+      top: 30,
+      left: 'center',
+      z: 100
+    },
+    tooltip: {
+      show: true,
+      trigger: 'axis'
+    }
+  };
+
+  water.setOption(option);
+  return water;
+}
 function initChart(canvas, width, height) {
-  const chart = echarts.init(canvas, null, {
+  widthL = width;
+  heightL = height;
+  chart = echarts.init(canvas, null, {
     width: width,
     height: height
   });
@@ -15,9 +48,9 @@ function initChart(canvas, width, height) {
       text: '实时风速查看',
       left: 'center'
     },
-    color: ["#37A2DA", "#67E0E3", "#9FE6B8"],
+    color: ["#37A2DA"],
     legend: {
-      data: ['港务集团', '长江防汛处'],
+      data: ['港务集团'],
       top: 50,
       left: 'center',
       z: 100
@@ -29,11 +62,43 @@ function initChart(canvas, width, height) {
       show: true,
       trigger: 'axis'
     },
-    xAxis: {
-      type: 'category',
-      boundaryGap: false,
-      data: ['周一', '周二', '周三', '周四', '周五', '周六', '周日'],
+    yAxis: {
+      x: 'center',
+      type: 'value',
+      splitLine: {
+        lineStyle: {
+          type: 'dashed'
+        }
+      }
       // show: false
+    }
+  };
+
+  chart.setOption(option);
+  return chart;
+}
+function initCjChart(canvas, width, height) {
+  chartCj = echarts.init(canvas, null, {
+    width: width,
+    height: height
+  });
+  canvas.setChart(chartCj);
+
+  var option = {
+  
+    color: ["#37A2DA"],
+    legend: {
+      data: ['长江防汛处'],
+      top: 50,
+      left: 'center',
+      z: 100
+    },
+    grid: {
+      containLabel: true
+    },
+    tooltip: {
+      show: true,
+      trigger: 'axis'
     },
     yAxis: {
       x: 'center',
@@ -44,24 +109,31 @@ function initChart(canvas, width, height) {
         }
       }
       // show: false
-    },
-    series: [{
-      name: '港务集团',
-      type: 'line',
-      smooth: true,
-      data: [18, 36, 65, 30, 78, 40, 33]
-    }, {
-      name: '长江防汛处',
-      type: 'line',
-      smooth: true,
-      data: [12, 50, 51, 35, 70, 30, 20]
-    }]
+    }
   };
 
-  chart.setOption(option);
-  return chart;
+  chartCj.setOption(option);
+  return chartCj;
 }
-
+function getMin(y1,y2,y3){
+  var min = y1[0]
+  for(var i=0;i<y1.length;i++){
+     if(y1[i]<min){
+       min = y1[i]
+     }
+  }
+  for (var i = 0; i < y2.length; i++) {
+    if (y2[i] < min) {
+      min = y2[i]
+    }
+  }
+  for (var i = 0; i < y3.length; i++) {
+    if (y3[i] < min) {
+      min = y3[i]
+    }
+  }
+  return min;
+}
 Page({
   onShareAppMessage: function (res) {
     return {
@@ -74,8 +146,14 @@ Page({
   data: {
     ec: {
       onInit: initChart
+    }, 
+    eccj: {
+      onInit: initCjChart
     },
-    tabs: ["选项一", "风速", "选项三"],
+    ecwater:{
+      onInit: initWater
+    },
+    tabs: ["选项一", "风速", "水位"],
     activeIndex: 1,
     sliderOffset: 0,
     sliderLeft: 0
@@ -83,7 +161,7 @@ Page({
 
   onReady() {
     setTimeout(this.getData, 500);
-  },
+  }, 
   onLoad: function () {
     var that = this;
     wx.getSystemInfo({
@@ -96,32 +174,183 @@ Page({
     });
   },
   tabClick: function (e) {
+    //console.log(e.currentTarget.id);
     this.setData({
-      sliderOffset: e.currentTarget.offsetLeft,
+      sliderOffset: e.currentTarget.offsetLeft, 
       activeIndex: e.currentTarget.id
     });
+    
+    //console.log(this)
+    //this.getData("2")
+
+   // setTimeout(this.getData(e.currentTarget.id), 500);
+
   },
   //getData方法里发送ajax
   getData() {
     wx.showLoading({
       title: '加载中...',
     })
+      wx.request({
+        url: 'http://140.143.26.197/api/show_wind',
+        herder: {
+          "content-type": "json"
+        },
+        success: function (res) {
+          console.log(res);
+          var data = res.data;
+          var x = []
+          var y = []
+          for (var i = 0; i < data.length; i++) {
+            x.push(data[i].x.substring(10, 16));
+            y.push(data[i].y);
+          }
+          chart.setOption({
+            xAxis: {
+              type: 'category',
+              boundaryGap: false,
+              data: x,
+              // show: false
+            },
+            series: [{
+              name: '港务集团',
+              type: 'line',
+              smooth: true,
+              data: y
+            }]
+          });
+          console.log(chart)
+          wx.hideLoading();
+        },
+        fail: function (res) {
+        },
+        complete: function (res) {
+        },
+      }) 
+      wx.request({
+        url: 'http://140.143.26.197/api/show_wind_scrapy',
+        herder: {
+          "content-type": "json"
+        },
+        success: function (res) {
+          console.log(res);
+          var data = res.data;
+          var x = []
+          var y = []
+          for (var i = 0; i < data.length; i++) {
+            x.push(data[i].x.substring(10, 13));
+            y.push(data[i].y);
+          }
+
+          chartCj.setOption({
+            xAxis: {
+              type: 'category',
+              boundaryGap: false,
+              data: x,
+              // show: false
+            },
+            series: [{
+              name: '长江防汛处',
+              type: 'line',
+              smooth: true,
+              data: y
+            }]
+          });
+          wx.hideLoading();
+        },
+        fail: function (res) {
+        },
+        complete: function (res) {
+        },
+      }) 
+
+      var y1 = []
+      wx.request({
+        url: 'http://140.143.26.197/api/show_water',
+        herder: {
+          "content-type": "json"
+        },
+        success: function (res) {
+          console.log(res);
+          var data = res.data;
+         
+          for (var i = 0; i < data.length; i++) {
+            y1.push(Number(data[i].y)*100);
+          }
+         
+        
+          wx.hideLoading();
+        },
+        fail: function (res) {
+        },
+        complete: function (res) {
+        },
+      }) 
     wx.request({
-      url: 'http://140.143.26.197/api/show_wind',
+      url: 'http://140.143.26.197/api/show_tide?type=jy',
       herder: {
         "content-type": "json"
       },
       success: function (res) {
         console.log(res);
-        console.log("111")
+        var data = res.data;
+        var x = []
+        var y2 = []
+        var y3 = []
+        for (var i = 0; i < data.length; i++) {
+          if(data[i].s == "1"){
+            x.push(data[i].x.substring(10, 13));
+            y2.push(data[i].y);
+          }else{
+            y3.push(data[i].y);
+          }
+        }
+       console.log(y3)
+        water.setOption({
+          xAxis: {
+            type: 'category',
+            boundaryGap: false,
+            data: x,
+            // show: false
+          },
+          series: [{
+            name: '港务集团水位',
+            type: 'line',
+            smooth: true,
+            data: y1
+          }, {
+            name: '江阴潮汐',
+            type: 'line',
+            smooth: true,
+            data: y2
+            }, {
+              name: '天生港潮汐',
+              type: 'line',
+              smooth: true,
+              data: y3,
+              z:100
+            }],
+          yAxis: {
+            x: 'center',
+            type: 'value',
+            splitLine: {
+              lineStyle: {
+                type: 'dashed'
+              }
+            },
+            min:getMin(y1,y2,y3)
+            // show: false
+          }
+        });
         wx.hideLoading();
       },
-      fail: function (res) { 
-        console.log("222")
+      fail: function (res) {
       },
       complete: function (res) {
-        console.log("444")
       },
-    })
-  }
+    }) 
+
+    }
+    
+  
 });
